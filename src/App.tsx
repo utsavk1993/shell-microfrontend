898e0f4423da0c8364ef6f99130d6db846f04503
Utsav Kumar <utsav.kumar@highspot.com>
2024-11-07 16:13:40 -0800

    Bootstrapped the shell app with create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Hello, This is the shell container!</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>React + Vite</li>
+          <li>Angular</li>
+          <li>Svelte</li>
+        </ul>
+      </nav>
+      <main>
+        <p>Click on the links above to see the different frameworks in action!</p>
+      </main>
+    </>
+  );
+};
+
+export default App;
